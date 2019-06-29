938dd7012a3854e9567c937f91391a448d0f15bc
Vijay Yadav <36539650+vijayyadava@users.noreply.github.com>
2019-06-29 22:43:32 +0530

    Pipeline test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Vijay. Welcome to Azure World!");
+    response.end("Hello Vijay. Welcome to Azure Pipeline World!");
 });
 
 const port = process.env.PORT || 1337;
